9ef2de45eafe7f260a1dcecc20e8db6dbe982e70
ZclipseCode <bmichaelbusiness@gmail.com>
2023-11-03 19:23:29 -0500

    removed dummy assets

---
+++ KeysToThePast/Assets/Brian/Scripts/CustomCharacter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCharacter : MonoBehaviour
+{
+    // include custom moveset
+
+    [SerializeField] GameObject hat;
+    [SerializeField] Color shirtColor;
+    [SerializeField] Color pantsColor;
+
+    [SerializeField] Transform hatPosition;
+    //[SerializeField] Mesh
+}
