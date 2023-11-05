ce34f707d10bce1cf06f2c900cf352b2478ca72f
Atlas1004 <119359076+Atlas1004@users.noreply.github.com>
2023-11-05 13:00:12 -0600

    yup

--- KeysToThePast/Assets/Ethan/HurtBox.cs
+++ KeysToThePast/Assets/Ethan/HurtBox.cs
@@ -6,16 +6,16 @@ public class HurtBox : MonoBehaviour{
     [SerializeField] int damage;
     [SerializeField] PlayerAttackStates pas1;
     private void OnTriggerEnter(Collider other) {
-        //if (other.GetComponent<Player>()) { //if the other player is hit
-        if (other.GetComponent<PlayerAttackStates>().attackState == CombatState.LOWBLOCK && (pas1.attackState == CombatState.MIDDLEATTACK || pas1.attackState == CombatState.LOWATTACK)) {
-            other.GetComponent<Health>().Block();
+        if (other.GetComponent<Player>().playerId != gameObject.GetComponent<Player>().playerId) { //if the other player is hit
+            if (other.GetComponent<PlayerAttackStates>().attackState == CombatState.LOWBLOCK && (pas1.attackState == CombatState.MIDDLEATTACK || pas1.attackState == CombatState.LOWATTACK)) {
+                other.GetComponent<Health>().Block();
+            }
+            else if (other.GetComponent<PlayerAttackStates>().attackState == CombatState.STANDINGBLOCK && (pas1.attackState == CombatState.OVERHEAD || pas1.attackState == CombatState.MIDDLEATTACK)) {
+                other.GetComponent<Health>().Block();
+            }
+            else {
+                other.GetComponent<Health>().TakeDamage(damage);
+            }
         }
-        else if (other.GetComponent<PlayerAttackStates>().attackState == CombatState.STANDINGBLOCK && (pas1.attackState == CombatState.OVERHEAD || pas1.attackState == CombatState.MIDDLEATTACK)) {
-            other.GetComponent<Health>().Block();
-        }
-        else {
-            other.GetComponent<Health>().TakeDamage(damage);
-        }
-       // }
     }
 }
