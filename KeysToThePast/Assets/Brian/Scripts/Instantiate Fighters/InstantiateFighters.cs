b3a3626d0eb5244ba48dad851cff909da8402134
ZclipseCode <bmichaelbusiness@gmail.com>
2023-11-09 03:42:20 -0600

    arena work

--- KeysToThePast/Assets/Brian/Scripts/Instantiate Fighters/InstantiateFighters.cs
+++ KeysToThePast/Assets/Brian/Scripts/Instantiate Fighters/InstantiateFighters.cs
@@ -9,6 +9,18 @@ public class InstantiateFighters : MonoBehaviour
     [SerializeField] Transform[] spawnLocations = new Transform[2];
     [SerializeField] GameObject eightesGuy;
 
+    [Header("Environments")]
+    [SerializeField] GameObject caveman;
+    [SerializeField] GameObject knight;
+    [SerializeField] GameObject brAIn;
+    [SerializeField] GameObject timeTraveler;
+    [SerializeField] GameObject cavemanEnvironement;
+    [SerializeField] GameObject knightEnvironment;
+    [SerializeField] GameObject eightesGuyEnvironment;
+    [SerializeField] GameObject brAInEnvironment;
+    [SerializeField] GameObject timeTravelerEnvironment;
+
+
     private void Start()
     {
         CreateFighters();
@@ -29,7 +41,7 @@ public class InstantiateFighters : MonoBehaviour
         player1Info.info = SelectFighter.players[0];
 
         GameObject player2;
-        if (SelectFighter.players[0].fighter == eightesGuy)
+        if (SelectFighter.players[1].fighter == eightesGuy)
         {
             player2 = Instantiate(SelectFighter.players[1].fighter, spawnLocations[1].position, Quaternion.Euler(0, 180, 0));
         }
@@ -42,5 +54,37 @@ public class InstantiateFighters : MonoBehaviour
 
         CameraFollowPlayers.addPlayerToCamera(player1.transform);
         CameraFollowPlayers.addPlayerToCamera(player2.transform);
+    }
+
+    public void CreateEnvironment(GameObject fighter, Transform position)
+    {
+        GameObject environement;
+
+        if (fighter == caveman)
+        {
+            environement = cavemanEnvironement;
+        }
+        else if (fighter == knight)
+        {
+            environement = knightEnvironment;
+        }
+        else if (fighter == eightesGuy)
+        {
+            environement = eightesGuyEnvironment;
+        }
+        else if (fighter = brAIn)
+        {
+            environement = brAInEnvironment;
+        }
+        else if (fighter == timeTraveler)
+        {
+            environement = timeTravelerEnvironment;
+        }
+        else
+        {
+            environement = cavemanEnvironement;
+        }
+
+        Instantiate(environement, position.position, Quaternion.identity);
     }
 }
